598b0ab5b988b3ad269b3ad2a441aca4c4a0af1b
sungju-kim <wnwn0719@gmail.com>
2021-11-04 14:42:05 +0900

    build printLadder

--- ladderGame/ladderGame/main.swift
+++ ladderGame/ladderGame/main.swift
@@ -14,10 +14,52 @@ struct Ladder {
         if player < 2 || player > 8 {
             return
         }
-        print(player)
+        printLadder(player: player)
+    }
+    func printLadder(player : Int){
+        let row = player
+        var ladderArr = Array(repeating: Array(repeating: "|", count: row), count: 5)
+        for i in 0...4 {
+            if i != 4 {
+                for j in 0...row-1{
+                    let orNot = Int.random(in: 0...1)
+                    let randomNum1 = randomNum(player: player, orNot: orNot)
+                    let randomNum2 = randomNum(player: player, orNot: orNot)
+                    if j == randomNum1 || j == randomNum2 {
+                        print(ladderArr[i][j], terminator: "-")
+                    }else{
+                        print(ladderArr[i][j], terminator: " ")
+                    }
+                }
+                print("")
+            }else{
+                let winning = Int.random(in: 0...player-1)
+                for j in 0...row-1{
+                    if j == winning {
+                        print("o", terminator: " ")
+                    }else{
+                        print("x", terminator: " ")
+                    }
+                }
+            }
+        }
+    }
+    func randomNum(player : Int, orNot : Int) -> Int {
+        var randomNum : Int = 0
+        let between = player - 2
+        if player < 4 {
+            if orNot == 1 {
+                randomNum = Int.random(in: 0...between)
+            }else {
+                randomNum = player
+            }
+        }else{
+            randomNum = Int.random(in: 0...between)
+        }
+        return randomNum
     }
 }
+print("인원수를 입력하세요 ( 2 ~ 8 ) ex) 5")
 var playerNum = Int(readLine() ?? "0")
 var ladder  = Ladder()
-
 ladder.playLadder(player: playerNum ?? 0 )
