fceff296e5c01e41fb2f430d6962c7dfc80471e5
sungju-kim <wnwn0719@gmail.com>
2021-11-09 18:57:55 +0900

    measure

--- Measure/Measure/ViewController.swift
+++ Measure/Measure/ViewController.swift
@@ -9,17 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     @IBOutlet weak var typeValue: UITextField!
+    @IBOutlet weak var showValue: UILabel!
+    @IBOutlet weak var beforeNum: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func convertButton(_ sender: Any) {
+    @IBAction func convertButton(_ value: Any) {
+        let value = typeValue.text ?? "0"
+        let double = Double(value) ?? 0.0
+        let beforeConvert = Measurement(value: double, unit: UnitLength.centimeters)
+        let afterConvert = beforeConvert.converted(to: UnitLength.meters)
+        
+        beforeNum.text = "\(beforeConvert)"
+        showValue.text = "\(afterConvert)"
+        typeValue.text = ""
         
     }
     
-    @IBOutlet weak var showValue: UILabel!
-
+    
 }
 
