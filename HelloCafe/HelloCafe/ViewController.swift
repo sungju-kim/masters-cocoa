fd03065c1216d0d2c7cf364745611c4288fd28b9
sungju-kim <wnwn0719@gmail.com>
2021-11-09 16:36:04 +0900

    add image and button

--- HelloCafe/HelloCafe/ViewController.swift
+++ HelloCafe/HelloCafe/ViewController.swift
@@ -9,11 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var menuImageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func menuButtonTouched(_ sender: Any) {
+        menuImageView.image = UIImage(named: "cafe-latte.jpg")
+    }
+    
 }
 
