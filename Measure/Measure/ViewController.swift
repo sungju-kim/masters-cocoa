ce4452613317188d87da63ec5a86e7726df037f4
sungju-kim <wnwn0719@gmail.com>
2021-11-09 17:06:37 +0900

    add label

--- Measure/Measure/ViewController.swift
+++ Measure/Measure/ViewController.swift
@@ -9,11 +9,17 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var typeValue: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func convertButton(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var showValue: UILabel!
 
 }
 
