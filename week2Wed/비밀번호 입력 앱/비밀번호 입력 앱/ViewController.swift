00eb2e6135b4ab90cb48567ccf9ab86545ee91bc
sungju-kim <wnwn0719@gmail.com>
2021-11-10 15:26:00 +0900

    solve

--- week2Wed/비밀번호 입력 앱/비밀번호 입력 앱/ViewController.swift
+++ week2Wed/비밀번호 입력 앱/비밀번호 입력 앱/ViewController.swift
@@ -14,48 +14,68 @@ class ViewController: UIViewController {
     @IBOutlet weak var descriptionLabel: UILabel!
     
     func passwordValidator(password:String) -> Int {
-            let passwordArr = password.map{($0)}
-            let passwordLength = passwordArr.count
-            var countNum = 0
-            var countWord = 0
-            var countSymbol = 0
-            for i in 0...(passwordLength-1) {
-                if(passwordArr[i].asciiValue! >= 97) && (passwordArr[i].asciiValue! <= 122) || (passwordArr[i].asciiValue! >= 65) && (passwordArr[i].asciiValue! <= 90) {  //영문자
-                    countWord += 1
-                }else if (passwordArr[i].asciiValue! >= 48) && (passwordArr[i].asciiValue! <= 57) { //숫자
-                    countNum += 1
-                    
-                }else if (passwordArr[i].asciiValue! >= 33) && (passwordArr[i].asciiValue! <= 126) { // 특수문자
-                    countSymbol += 1
-                }
+        let passwordArr = password.map{($0)}
+        let passwordLength = passwordArr.count
+        var countNum = 0
+        var countWord = 0
+        var countSymbol = 0
+        for i in 0...(passwordLength-1) {
+            if(passwordArr[i].asciiValue! >= 97) && (passwordArr[i].asciiValue! <= 122) || (passwordArr[i].asciiValue! >= 65) && (passwordArr[i].asciiValue! <= 90) {  //영문자
+                countWord += 1
+            }else if (passwordArr[i].asciiValue! >= 48) && (passwordArr[i].asciiValue! <= 57) { //숫자
+                countNum += 1
+                
+            }else if (passwordArr[i].asciiValue! >= 33) && (passwordArr[i].asciiValue! <= 126) { // 특수문자
+                countSymbol += 1
             }
-            if (countNum > 1) && (countWord > 1) && (countSymbol > 1 ) && (passwordLength >= 8) {
-                return 5
-            }else if (countWord >= 1) && (countNum >= 1) && (passwordLength >= 8) {
-                return 4
-            }else if (countWord > 1) && (countWord == 0) && (passwordLength < 8) {
-                return 2
-            }else if (countNum == passwordLength) && (passwordLength < 8) {
-                return 1
-            }else {
-                return 3
-            }
-            
         }
-        func printResult() {
-            guard let passwordInput = passwordInput.text else { return  }
-            print(passwordInput)
-            print(passwordValidator(password: passwordInput))
+        if (countNum > 1) && (countWord > 1) && (countSymbol > 1 ) && (passwordLength >= 8) {
+            return 5
+        }else if (countWord >= 1) && (countNum >= 1) && (passwordLength >= 8) {
+            return 4
+        }else if (countWord > 1) && (countWord == 0) && (passwordLength < 8) {
+            return 2
+        }else if (countNum == passwordLength) && (passwordLength < 8) {
+            return 1
+        }else {
+            return 3
         }
+        
+    }
+
+    func printColorOf(passwordGrade : Int) {
+        switch passwordGrade {
+        case 1 :
+            levelView.backgroundColor = .red
+            descriptionLabel.text = "아주 약한 암호"
+        case 2 :
+            levelView.backgroundColor = .orange
+            descriptionLabel.text = "약한 암호"
+        case 3 :
+            levelView.backgroundColor = .yellow
+            descriptionLabel.text = "보통 암호"
+        case 4 :
+            levelView.backgroundColor = .blue
+            descriptionLabel.text = "강한 암호"
+        case 5 :
+            levelView.backgroundColor = .green
+            descriptionLabel.text = "아주 강한 암호"
+        default:
+            levelView.backgroundColor = .secondarySystemBackground
+            descriptionLabel.text = "암호 수준"
+        }
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
     @IBAction func verifyBtn(_ sender: Any) {
+        guard let passwordInput = passwordInput.text else { return  }
+        printColorOf(passwordGrade: passwordValidator(password: passwordInput))
     }
 }
 
