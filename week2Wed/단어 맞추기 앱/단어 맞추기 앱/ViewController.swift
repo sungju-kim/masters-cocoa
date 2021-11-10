d1aec9e65f2954933189af4d00dabfbb795e2e7d
sungju-kim <wnwn0719@gmail.com>
2021-11-10 17:54:01 +0900

    add return key response

--- week2Wed/단어 맞추기 앱/단어 맞추기 앱/ViewController.swift
+++ week2Wed/단어 맞추기 앱/단어 맞추기 앱/ViewController.swift
@@ -7,33 +7,33 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nowWord: UILabel!
     @IBOutlet weak var typeWord: UITextField!
     @IBOutlet weak var nextWord: UILabel!
     
     var wordArr = ["사과","바나나","포도","딸기","파인애플","복숭아","수박","망고","블루베리","배"]
-
-        func newGame() {
-            wordArr.shuffle()
-            printLabel()
+    
+    func newGame() {
+        wordArr.shuffle()
+        printLabel()
+    }
+    
+    func printLabel() {
+        switch wordArr.count{
+        case 0...1:
+            nowWord.text = ""
+            nextWord.text = ""
+        case 2:
+            nowWord.text = wordArr[0]
+            nextWord.text = ""
+        case 3...:
+            nowWord.text = wordArr[0]
+            nextWord.text = wordArr[1]
+        default:
+            0
         }
-
-        func printLabel() {
-            switch wordArr.count{
-            case 0...1:
-                nowWord.text = ""
-                nextWord.text = ""
-            case 2:
-                nowWord.text = wordArr[0]
-                nextWord.text = ""
-            case 3...:
-                nowWord.text = wordArr[0]
-                nextWord.text = wordArr[1]
-            default:
-                0
-            }
-        }
+    }
     func checkWord(word : String) -> Int {
         while wordArr.count > 0{
             if word == nowWord.text{
@@ -44,18 +44,25 @@ class ViewController: UIViewController {
         }
         return 3
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         newGame()
         let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
         view.addGestureRecognizer(tap)
+        self.typeWord.delegate = self
         // Do any additional setup after loading the view.
     }
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
-
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        if textField == self.typeWord {
+            self.submitBtn(self)
+        }
+        return true
+    }
+    
     @IBAction func submitBtn(_ sender: Any) {
         guard let typedWord = typeWord.text else{return}
         switch checkWord(word: typedWord) {
