4e326bf84786fddbd8a0fd7be82bec22f5f9b5a0
sungju-kim <wnwn0719@gmail.com>
2021-11-11 17:16:19 +0900

    함수분리

--- week2Wed/단어 맞추기 앱/단어 맞추기 앱/ViewController.swift
+++ week2Wed/단어 맞추기 앱/단어 맞추기 앱/ViewController.swift
@@ -13,12 +13,18 @@ class ViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var nextWord: UILabel!
     
     var wordArr = ["사과","바나나","포도","딸기","파인애플","복숭아","수박","망고","블루베리","배"]
-    
-    func newGame() {
-        wordArr.shuffle()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        createWordArr()
         printLabel()
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+        self.typeWord.delegate = self
+        // Do any additional setup after loading the view.
     }
-    
+    func createWordArr() {
+        wordArr.shuffle()
+    }
     func printLabel() {
         switch wordArr.count{
         case 0:
@@ -31,8 +37,19 @@ class ViewController: UIViewController, UITextFieldDelegate {
             nowWord.text = wordArr[0]
             nextWord.text = wordArr[1]
         default:
-            0
+            break
         }
+    }
+    
+    func checkWord(word : String) -> Int {
+        while wordArr.count > 0{
+            if word == nowWord.text{
+                return 1
+            }else if word != nowWord.text{
+                return 0
+            }
+        }
+        return 3
     }
     
     @IBAction func submitBtn(_ sender: Any) {
@@ -51,25 +68,13 @@ class ViewController: UIViewController, UITextFieldDelegate {
         }
     }
     
-    func checkWord(word : String) -> Int {
-        while wordArr.count > 0{
-            if word == nowWord.text{
-                return 1
-            }else if word != nowWord.text{
-                return 0
-            }
-        }
-        return 3
+    @IBAction func rePlay(_ sender: Any) {
+        wordArr = ["사과","바나나","포도","딸기","파인애플","복숭아","수박","망고","블루베리","배"]
+        createWordArr()
+        printLabel()
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        newGame()
-        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
-        view.addGestureRecognizer(tap)
-        self.typeWord.delegate = self
-        // Do any additional setup after loading the view.
-    }
+    
     @objc func dismissKeyboard() {
         view.endEditing(true)
     }
@@ -81,9 +86,5 @@ class ViewController: UIViewController, UITextFieldDelegate {
     }
     
     
-    @IBAction func rePlay(_ sender: Any) {
-        wordArr = ["사과","바나나","포도","딸기","파인애플","복숭아","수박","망고","블루베리","배"]
-        newGame()
-    }
 }
 
