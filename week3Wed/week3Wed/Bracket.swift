1bca11bd3a71545fd3fb69c6879154e20d9d7596
sungju-kim <wnwn0719@gmail.com>
2021-11-17 14:41:44 +0900

    solve bracket

---
+++ week3Wed/week3Wed/Bracket.swift
@@ -0,0 +1,63 @@
+//
+//  Bracket.swift
+//  week3Wed
+//
+//  Created by 성주 on 2021/11/17.
+//
+
+import Foundation
+
+struct Brackets {
+    enum Bracket :String{
+        case round = "()"
+        case square = "[]"
+        case curly = "{}"
+    }
+    var leftBrk : String
+    var rightBrk : String
+    init(_ string : Bracket) {
+        switch string{
+        case .square:
+            leftBrk = "["
+            rightBrk = "]"
+        case .curly:
+            leftBrk = "{"
+            rightBrk = "}"
+        default:
+            leftBrk = "("
+            rightBrk = ")"
+        }
+    }
+    
+    func generate(with count:Int) -> Array<String> {
+        var arr : [String] = []
+        if count == 0 {
+            arr.append("")
+        }
+        for index in 0..<count{
+            for left in generate(with: index) {
+                for right in generate(with: count-index-1) {
+                    arr.append("\(leftBrk)\(left)\(rightBrk)\(right)")
+                }
+            }
+        }
+        return arr
+    }
+    
+    func analyze(with value: String) -> Bool {
+        var count = 0
+        for char in value{
+            switch char{
+            case "(", "{", "[":
+                count += 1
+            case ")", "}", "]":
+                if count > 0{count -= 1}else{ return false}
+            default:
+               count += 0
+            }
+        }
+        return count == 0 ? true : false
+    }
+}
+
+
---
+++ week3Wed/week3Wed/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  week3Wed
+//
+//  Created by 성주 on 2021/11/17.
+//
+
+import Foundation
+
+//let bracket = Brackets(.round)
+//print(bracket.analyze(with: "())()(("))
+//print(bracket.generate(with: 3))
