8fab074338fd4f06cd69f19a3f2bd1fda56ee89e
sungju-kim <wnwn0719@gmail.com>
2021-11-24 11:54:00 +0900

    solve mission 1

---
+++ week4wed/week4wed/main.swift
@@ -0,0 +1,13 @@
+//
+//  main.swift
+//  week4wed
+//
+//  Created by 성주 on 2021/11/24.
+//
+
+import Foundation
+
+
+
+files.printResult("isExist", "/System/Library/Desktop Pictures", "Big Sur Aerial.madesktop,Big Sur Coastline.madesktop")
+
---
+++ week4wed/week4wed/연습문제 1.swift
@@ -0,0 +1,54 @@
+//
+//  연습문제 1.swift
+//  week4wed
+//
+//  Created by 성주 on 2021/11/24.
+//
+
+import Foundation
+
+struct Files {
+//    var path : String
+//    var filename : String
+//    init(path: String, filename: String) {
+//        self.path = path
+//        self.filename = filename
+//    }
+    func files(at path: String) -> Array<String> {
+        return try!FileManager.default.contentsOfDirectory(atPath: path).sorted()
+    }
+    
+    func isExist(filename: String, at path: String) -> Bool {
+        return files(at: path).contains(filename) ? true : false
+        
+    }
+    
+    
+    func findDoc(at path: String) -> Array<String> {
+        return files(at: path).filter{$0.contains(".doc")}
+    }
+    
+    func multiString(filename : String ,at path: String) -> [Bool]{
+        return filename.components(separatedBy: ",").map{isExist(filename: $0, at: path)}
+        
+    }
+    func printResult(_ method : String, _ path: String, _ filename: String) {
+        switch method {
+        case "files":
+            print(files(at: path))
+        case "isExist":
+            if filename.contains(","){
+                print(multiString(filename: filename, at: path))
+            }else{
+                print(isExist(filename: filename, at: path))
+            }
+        case "findDoc":
+            print(findDoc(at: path))
+        default:
+            print("nil")
+        }
+    }
+}
+
+let files = Files()
+
